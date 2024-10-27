6705479292b781e39c68c17ba30bbae308cac5ce
s13tc2 <kmk9957@outlook.com>
2024-10-27 17:44:24 -0400

    add secrets.tf

---
+++ src/terraform/infra/secrets.tf
@@ -0,0 +1,17 @@
+resource "aws_secretsmanager_secret" "database_connection_string" {
+  name        = "${var.application_name}-${var.environment_name}-connection-string"
+  description = "Database connection string"
+  recovery_window_in_days = 0  # This forces immediate deletion
+  force_overwrite_replica_secret = true
+}
+
+resource "aws_secretsmanager_secret_version" "database_connection_string" {
+  secret_id     = aws_secretsmanager_secret.database_connection_string.id
+  secret_string = random_password.database_connection_string.result
+}
+
+resource "random_password" "database_connection_string" {
+  length           = 16
+  special          = true
+  override_special = "!#$%&*()-_=+[]{}<>:?"
+}
