4ad010b71861307788fddf9c4fda4b8ff4410e6e
s13tc2 <kmk9957@outlook.com>
2024-10-27 17:51:43 -0400

    minor updates

--- src/terraform/infra/network-airflow.tf
+++ src/terraform/infra/network-airflow.tf
@@ -15,7 +15,7 @@ resource "aws_security_group" "airflow_vpc_endpoints" {
 # VPC Endpoints needed for Airflow
 resource "aws_vpc_endpoint" "s3" {
   vpc_id       = aws_vpc.main.id
-  service_name = "com.amazonaws.${var.region}.s3"
+  service_name = "com.amazonaws.${var.primary_region}.s3"
   vpc_endpoint_type = "Gateway"
   route_table_ids = [for rt in aws_route_table.backend : rt.id]
 }
