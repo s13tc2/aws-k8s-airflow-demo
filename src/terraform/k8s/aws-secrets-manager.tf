fcb9410f344218ad355dcfbf5b611375d5757385
s13tc2 <kmk9957@outlook.com>
2024-10-28 11:06:34 -0400

    change yaml

--- src/terraform/k8s/aws-secrets-manager.tf
+++ src/terraform/k8s/aws-secrets-manager.tf
@@ -47,7 +47,7 @@ resource "kubernetes_manifest" "secret_provider_class" {
     apiVersion = "secrets-store.csi.x-k8s.io/v1"
     kind = "SecretProviderClass"
     metadata = {
-      name = "${var.application_name}-${var.environment_name}-secret-provider-class"
+      name = "${var.application_name}-app-${var.environment_name}-secret-provider-class"
       namespace = var.k8s_namespace
     }
     spec = {
@@ -55,7 +55,7 @@ resource "kubernetes_manifest" "secret_provider_class" {
       parameters = {
         objects = yamlencode([
           {
-            objectName = "airflow-dev}-connection-string"
+            objectName = "airflow-dev-connection-string"
             objectType = "secretsmanager"
             objectVersionLabel = "AWSCURRENT"
           },
