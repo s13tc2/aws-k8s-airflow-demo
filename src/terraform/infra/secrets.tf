ec790c4a8eeff25861b66c2f684f964d65f4afc6
s13tc2 <kmk9957@outlook.com>
2024-10-28 16:18:04 -0400

    dockerfile

--- src/terraform/infra/secrets.tf
+++ src/terraform/infra/secrets.tf
@@ -15,3 +15,24 @@ resource "random_password" "database_connection_string" {
   special          = true
   override_special = "!#$%&*()-_=+[]{}<>:?"
 }
+
+resource "aws_secretsmanager_secret" "fernet_key" {
+  name = "${var.application_name}-${var.environment_name}-fernet-key"
+  description = "Airflow fernet key for encryption"
+  recovery_window_in_days = 0 # This forces immediate deletion
+  force_overwrite_replica_secret = true
+}
+
+resource "aws_secretsmanager_secret_version" "fernet_key" {
+  secret_id = aws_secretsmanager_secret.fernet_key.id
+  secret_string = random_password.fernet_key.result
+}
+
+resource "random_password" "fernet_key" {
+  length = 32  # Fernet keys should be 32 bytes
+  special = true
+  override_special = "_-" # Limiting special characters for fernet key compatibility
+  upper = true
+  lower = true
+  number = true
+}
