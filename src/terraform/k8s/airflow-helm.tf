baa12d4f89947ca910e2d0948e026685ef6e45ed
s13tc2 <kmk9957@outlook.com>
2024-10-29 14:31:08 -0400

    just helm

--- src/terraform/k8s/airflow-helm.tf
+++ src/terraform/k8s/airflow-helm.tf
@@ -1,38 +1,13 @@
-# Create the Helm template
-data "helm_template" "airflow" {
-  name       = "airflow"
-  repository = "https://airflow.apache.org"
-  chart      = "airflow"
-  version    = "1.15.0"
-
-  values = [
-    yamlencode({
-      executor = "KubernetesExecutor"
-    
-    })
-  ]
-}
-
-# Save the rendered values
-resource "local_file" "airflow_values" {
-  content  = data.helm_template.airflow.values[0]  # Access the first element of the values list
-  filename = "${path.module}/values.yaml"
-}
-
-# Deploy Airflow
+# Deploy Airflow using default values
 resource "helm_release" "airflow" {
   name             = "airflow"
   repository       = "https://airflow.apache.org"
   chart            = "airflow"
+  version          = "1.15.0"
   namespace        = "airflow"
   create_namespace = true
-  
-  values = [
-    data.helm_template.airflow.values[0]  # Use values directly
-  ]
 
   # Add timeout and other helm configurations for stability
   timeout = 600
   atomic  = true
-  wait    = true
 }
