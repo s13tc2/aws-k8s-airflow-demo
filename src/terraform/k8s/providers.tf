13ce18a4bd356de4360788166e1c0b9f2e8db75c
s13tc2 <kmk9957@outlook.com>
2024-10-29 13:22:07 -0400

    update k8s-apply.yaml

---
+++ src/terraform/k8s/providers.tf
@@ -0,0 +1,17 @@
+provider "aws" {
+  region = var.primary_region
+}
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.main.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.main.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.main.token
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = data.aws_eks_cluster.main.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.main.certificate_authority[0].data)
+    token                  = data.aws_eks_cluster_auth.main.token
+  }
+}
